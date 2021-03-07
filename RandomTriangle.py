a067a75de2f2be58ed8aca2b6f7f3466e88787b0
Hooooot13 <Hooooot13@lcdx5000@gmail.com>
2021-03-07 14:25:09 -0600

    C`1 C C

--- RandomTriangle.py
+++ RandomTriangle.py
@@ -8,12 +8,12 @@ V1 = [0,0]
 V2 = [5,10]
 V3 = [10, 0]
 X1 = r.randint(0,11)
-X2 = r.randint(0.11)
+X2 = r.randint(0,11)
 X = [X1, X2]
 
-fig = figure()
-plot(V1, V2, V3)
-
+fig = plt.figure()
+plt.plot(V1, V2, V3)
+plt.savefig('testPlot.png')
 
 #def PickNewPoint(i=int):
 #    r.randint(1,4)
