7eb5f8a6c26d296f9a936687c655c230e9479228
Levi Carroll <lcdx5000@gmail.com>
2021-03-07 19:56:00 -0600

    commit

---
+++ RandomTriangle.py
@@ -0,0 +1,20 @@
+import matplotlib.animation as ani
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import random as r
+
+V1 = [0,0]
+V2 = [5,10]
+V3 = [10, 0]
+X1 = r.randint(0,11)
+X2 = r.randint(0.11)
+X = [X1, X2]
+
+fig = figure()
+plot(V1, V2, V3)
+
+
+#def PickNewPoint(i=int):
+#    r.randint(1,4)
+    
